4e379e9c95d465211b126ac2faf3187be1706a02
Pietro1711 <cannizzaro1711@gmail.com>
2019-02-04 17:51:23 +0100

    adding Unity Files

---
+++ Assets/_PC/Unity-Spiel/Quest 1/Scripts/VerticalMover.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalMover : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 500f;
+    public float sidewaysForce = 500f;
+    public float jumpForce = 500f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        
+
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+
+        }
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+
+        }
+
+        if (Input.GetKey("space"))
+        {
+            rb.AddForce(0, jumpForce * Time.maximumDeltaTime, 0);
+
+        }
+
+    }
+}
